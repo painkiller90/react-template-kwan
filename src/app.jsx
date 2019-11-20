ca344215059a4b82b4931ba2dd3c26cc817875cb
Antonio Aguilar <antonio.1avp@gmail.com>
2019-11-20 11:01:35 -0600

    table created in app component

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import './app.css';
 
 export default class componentName extends Component {
+
+  getRows = () => {
+    const { data } = this.props;
+    return data.map((row) => (
+      <tr key={row.id}>
+        {row.values.map((values, index) => (<td key={index}>{values}</td>))}
+      </tr>
+    ));
+  }
+
   render() {
     return (
-      <div> React + Webpack + Babel = ❤️ </div>
+      <table className="react-table">
+        <tbody>
+          {this.getRows()}
+        </tbody>
+      </table>
     );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
+const dataTable = [
+    {
+        id: 1,
+        values: ['1', 'Bill', 'Gates']
+    },
+    {
+        id: 2,
+        values: ['2', 'Jeff', 'Bezos']
+    },
+    {
+        id: 3,
+        values: ['3', 'Mark', 'Zuckerberg']
+    }
+];
+
 ReactDOM.render(
-    <App />,
+    <App data={dataTable} />,
     document.getElementById('root')
 );
