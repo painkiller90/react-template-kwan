9fc1ad92529b3f7e9a20e421ea2cf556ac25cc5e
Antonio Aguilar <antonio.1avp@gmail.com>
2019-11-20 10:07:04 -0600

    Some babel configurations added

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class componentName extends Component {
   render() {
     return (
-      <div> textInComponent </div>
+      <div> React + Webpack + Babel = ❤️ </div>
     );
   }
 }
